356a32299a933985e765c124d78fb62529db0c31
fernando <fernando24164@gmail.com>
2018-07-22 22:05:14 +0200

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import React from "react"
-import {Container, Alert} from "reactstrap"
+import {Container, Alert, Row, Col, InputGroup, Input, Button} from "reactstrap"
 
 class App extends React.Component{
     render(){
         return(
             <Container>
-                <Alert color="success"><h1>Test!</h1></Alert>
+                <Row>
+                    <Col xs="12" md="12" lg="12">
+                        <Alert color="success" className="text-center"><h1>Todo App</h1></Alert>
+                    </Col>
+                </Row>
+                <Row>
+                    <InputGroup>
+                        <Input placeholder="Add Todo"/>
+                        <Button color="success">Add Todo</Button>
+                    </InputGroup>
+                </Row>
             </Container>
         )
     }
